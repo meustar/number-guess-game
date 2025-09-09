f5e9400ca886bd5df095b792ba350bd3edd569ce
meustar <meustar829@gmail.com>
2025-09-10 02:59:20 +0900

    리셋 버튼 활성화, 게임 오버시 게임오버 메세지 호출

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 // 1. 랜덤번호 지정
 
-let computerNum = 0;
-
-function picRandomNum() {
-  computerNum = Math.floor(Math.random() * 100) + 1;
-  console.log("정답", computerNum);
-}
-
-picRandomNum();
-
 // 2. 유저가 번호를 입력한다. 그리고 go 라는 버튼을 누름
 
 // 3. 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
@@ -25,25 +16,26 @@ picRandomNum();
 
 // 9. 유저가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회를 깎지 않는다.
 
-// index.html
-
+let computerNum = 0;
 let playButton = document.getElementById("play-button");
-
-// console.log(playButton);
 let userInput = document.getElementById("user-input");
-
 let resultArea = document.getElementById("result-area");
-
+let resetButton = document.getElementById("reset-button");
 let chances = 5;
 let gameOver = false;
 let chanceArea = document.getElementById("chance-area");
 let history = [];
 
 playButton.addEventListener("click", play);
-
+resetButton.addEventListener("click", reset);
 userInput.addEventListener("focus", function () {
   userInput.value = "";
 });
+
+function pickRandomNum() {
+  computerNum = Math.floor(Math.random() * 100) + 1;
+  console.log("정답", computerNum);
+}
 
 function play() {
   // console.log("게임 시작");
@@ -80,8 +72,9 @@ function play() {
   history.push(userValue);
   console.log(history);
 
-  if (chances < 1) {
+  if (chances == 0) {
     gameOver = true;
+    resultArea.textContent = "저런... G A M E O V E R !!";
   }
 
   if (gameOver == true) {
@@ -89,16 +82,19 @@ function play() {
   }
 }
 
-let resetButton = document.getElementById("reset-button");
-resetButton.addEventListener("click", reset);
-
 function reset() {
+  // 새로운 번호가 생성
+  pickRandomNum();
   // user input 창이 깨끗하게 정리되고.
   userInput.value = "";
 
-  // 새로운 번호가 생성
-  picRandomNum();
-
+  gameOver = false;
+  playButton.disabled = false;
+  chances = 5;
+  chanceArea.textContent = `남은기회: ${chances}번`;
   //결과창이 바뀐다.
   resultArea.textContent = "결과";
+  userValueList = [];
 }
+
+pickRandomNum();
